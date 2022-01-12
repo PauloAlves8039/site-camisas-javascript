81c51f65564e27153fd8ab5ae36f23d04202c8df
PauloAlves8039 <pj38alves@gmail.com>
2022-01-12 20:25:10 -0300

    Adicionando projeto no repositório

---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+var camisetas = {
+    'branca': {
+        
+        'gola_v': {
+            'sem_estampa': {
+                'preco_unit': 5.12,
+                'foto': 'v-white.jpg' 
+            },
+            'com_estampa': {
+                'preco_unit': 8.95,
+                'foto': 'v-white-personalized.jpg' 
+            }
+        },
+        
+        'gola_normal': {
+            'sem_estampa': {
+                'preco_unit': 4.99,
+                'foto': 'normal-white.jpg' 
+            },
+            'com_estampa': {
+                'preco_unit': 8.77,
+                'foto': 'normal-white-personalized.jpg' 
+            }
+        }
+    },
+    
+    'colorida': {
+        'gola_v': {
+            'sem_estampa': {
+                'preco_unit': 6.04,
+                'foto': 'v-color.jpg' 
+            },
+            'com_estampa': {
+                'preco_unit': 9.47,
+                'foto': 'v-color-personalized.png' 
+            }
+        },
+        
+        'gola_normal': {
+            'sem_estampa': {
+                'preco_unit': 5.35,
+                'foto': 'normal-color.jpg' 
+            },
+            'com_estampa': {
+                'preco_unit': 9.28,
+                'foto': 'normal-color-personalized.jpg' 
+            }
+        }
+    }
+}
+
+var parametros_pesquisa = {
+    "quantidade": 10,
+    "cor": "colorida",
+    "gola": "gola_v",
+    "qualidade": "q150",
+    "estampa": "com_estampa",
+    "embalagem": "bulk"
+}
+
+$(function(){
+
+});
